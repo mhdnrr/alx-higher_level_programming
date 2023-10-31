f1da93ae2af4fb6081295130903e2582431b6f63
mhdnrr <mhdnrr@gmail.com>
2023-10-31 10:30:24 -0700

    All

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-uppercase = __import__('8-uppercase').uppercase
+def uppercase(str):
+
+    for i in str:
+
+        if ord("a") <= ord(i) <= ord("z"):
+
+            i = chr(ord(i) - 32)
+
+        print("{:s}".format(i), end="")
+
+    print()
 
 
-uppercase("best")
 
-uppercase("Best School 98 Battery street")
