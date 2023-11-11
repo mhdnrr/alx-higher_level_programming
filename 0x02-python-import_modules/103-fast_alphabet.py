73ea50c47b97d8078236b76a0868fcfb30afeb08
mhdnrr <mhdnrr@gmail.com>
2023-11-11 03:36:55 -0800

    All

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,22 +1,3 @@
+#!/usr/bin/python3
 import string
-
-def generate_alphabet():
-    alphabet = string.ascii_uppercase
-    index = 0
-    final_output = ""
-    
-    def generate_output():
-        nonlocal index
-        nonlocal final_output
-        
-        if index < len(alphabet):
-            final_output += alphabet[index]
-            index += 1
-            generate_output()
-    
-    generate_output()
-    
-    return final_output
-
-output = generate_alphabet()
-print(output)
+print(string.ascii_uppercase)
